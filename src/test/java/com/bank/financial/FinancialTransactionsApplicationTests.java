1e072ad9995e9f49ac141f3706f24a0e1d4332fb
Roberto Duessmann <roberto.duessmann@gmail.com>
2020-05-02 12:52:28 +0200

    Add testcontainers to mock tests

--- src/test/java/com/bank/financial/FinancialTransactionsApplicationTests.java
+++ src/test/java/com/bank/financial/FinancialTransactionsApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FinancialTransactionsApplicationTests {
+class FinancialTransactionsApplicationTests extends ItContextConfig {
 
 	@Test
 	void contextLoads() {
